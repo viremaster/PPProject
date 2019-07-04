ff253984e406352ffd5971306c7f95581bb714ff
ruben <theesmink@gmail.com>
2019-07-04 23:08:29 +0200

    yeet

--- src/main/java/project/test/CheckerTest.java
+++ src/main/java/project/test/CheckerTest.java
@@ -28,6 +28,23 @@ public class CheckerTest {
 	private final static String BASE_DIR = "\\src\\src\\main\\java\\project\\test\\TypeCheckertestFiles\\";
 	private final static String EXT = ".emoji";
 
+	//testing variables with the same name.
+	@Test
+	public void VarsWithSameNames() throws IOException, ParseException {
+		//all combinations of variables.
+		checkFail("VarSameNameGlobalLocal");
+		checkFail("VarSameNameGlobalLock");
+		checkFail("VarSameNameLocalGlobal");
+		checkFail("VarSameNameLocalLock");
+		checkFail("VarSameNameLockLocal");
+		checkFail("VarSameNameLockGlobal");
+		checkFail("VarSameNameLocalLocal");
+		checkFail("VarSameNameGlobalGlobal");
+		
+		//Special case multiple locks can be of the same name;
+		check("VarSameNameLockLock");
+	}
+	
 	//Scopes inside if and while
 	@Test
 	public void IfWhileScopes() throws IOException, ParseException {
@@ -71,7 +88,48 @@ public class CheckerTest {
 		
 	}
 	
+	//Global vars only in top level of main thread.
+	@Test
+	public void GlobalMainThreadTop() throws IOException, ParseException {
+		//global var in top of main thread.
+		check("Globalmaintop");
+		//global var in if.
+		checkFail("Globalif");
+		//global var in else part of if.
+		checkFail("Globalif2");
+		//global var in while.
+		checkFail("Globalwhile");
+		//global var in thread.
+		checkFail("GlobalThread");
+	}
 
+	//Global vars only in main thread.
+	@Test
+	public void JoinMainThreadTop() throws IOException, ParseException {
+		//join in top of main thread.
+		check("Joinmaintop");
+		//join in if.
+		check("Joinif");
+		//join in else part of if.
+		check("Joinif2");
+		//join in while.
+		check("Joinwhile");
+		//join in thread.
+		checkFail("JoinThread");
+	}
+	
+	//Global vars only in top level of main thread.
+		@Test
+		public void UnlockAfterLock() throws IOException, ParseException {
+		//unlock after a lock.
+		check("UnlockAfterLock");
+		//unlocking before a lock.
+		checkFail("UnlockBeforeLock");
+		//Locking in threads, if or while is ok as declaring variables is a compiler thing.
+		check("UnlockIf");
+		check("UnlockWhile");
+		check("UnlockThread");
+		}
 
 	private void checkFail(String filename) throws IOException {
 		try {
